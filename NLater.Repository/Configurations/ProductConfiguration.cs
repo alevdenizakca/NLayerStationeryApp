2771770137fdac37caadcc1add34f0a44a1aa592
alevdenizakca <alevdenizakca@gmail.com>
2023-04-24 18:11:12 +0300

    Add project files.

---
+++ NLater.Repository/Configurations/ProductConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core.Entities;
+
+namespace NLayer.Repository.Configurations
+{
+    internal class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id); //no needed but...
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x=> x.Name).HasMaxLength(200).IsRequired();
+            builder.Property(x => x.Stock).IsRequired();
+            builder.Property(x=>x.Price).IsRequired().HasColumnType("decimal(18,2)"); //################.## => total 18 number. 16 whole number, 2 decimal number
+            builder.ToTable("Product"); //no needed.
+            builder.HasOne(x=>x.Category).WithMany(x=>x.Products).HasForeignKey(x=>x.CategoryId); //no needed...
+            
+
+        }
+    }
+}
---
+++ NLater.Repository/Seeds/ProductSeed.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core.Entities;
+
+namespace NLayer.Repository.Seeds
+{
+    internal class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+
+            //Names are given as a parameter for constructor method
+            builder.HasData(
+            new Product("Dotring")
+            {
+                Id = 1,
+                CategoryId = 1,
+                Price = 100,
+                Stock = 20,
+                CreatedDate = DateTime.Now
+            },
+            new Product("Cabel Fastel")
+            {
+                Id = 2,
+                CategoryId = 1,
+                Price = 100,
+                Stock = 20,
+                CreatedDate = DateTime.Now
+            }, new Product("Farker")
+            {
+                Id = 3,
+                CategoryId = 1,
+                Price = 100,
+                Stock = 20,
+                CreatedDate = DateTime.Now
+            }, new Product("The Origin Of Species")
+            {
+                Id = 4,
+                CategoryId = 2,
+                Price = 100,
+                Stock = 20,
+                CreatedDate = DateTime.Now
+            }, new Product("Gone with the Wind")
+            {
+                Id = 5,
+                CategoryId = 2,
+                Price = 100,
+                Stock = 20,
+                CreatedDate = DateTime.Now
+            }, new Product("Killing the Mockingbird")
+            {
+                Id = 6,
+                CategoryId = 2,
+                Price = 100,
+                Stock = 20,
+                CreatedDate = DateTime.Now
+            }
+            );
+        }
+    }
+}
